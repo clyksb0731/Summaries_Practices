7b6d376df6a57cf261239fb2028b7911db77e9d0
Yongseok Choi <clyksb0731@gmail.com>
2018-05-17 23:47:11 +0900

    Create a new project for the first section in the book, Apprentice

--- Tutorials/Section_1/Section_1/ViewController.swift
+++ Tutorials/Section_1/Section_1/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Chpter_1-3
+//  Section_1
 //
-//  Created by 최용석 on 2018. 5. 16..
+//  Created by 최용석 on 2018. 5. 17..
 //  Copyright © 2018년 clyksb0731. All rights reserved.
 //
 
