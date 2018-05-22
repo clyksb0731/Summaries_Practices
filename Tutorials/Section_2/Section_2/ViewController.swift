cd48ff7693ce64b4948b4ac5f38ee613659806c4
Yongseok Choi <clyksb0731@gmail.com>
2018-05-22 23:08:56 +0900

    Add a new tutorial project, Section-2

---
+++ Tutorials/Section_2/Section_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Section_2
+//
+//  Created by 최용석 on 2018. 5. 22..
+//  Copyright © 2018년 clyksb0731. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
