2cc44a2bae4a0cd7eae845496d67d8ebd329b039
Yongseok Choi <clyksb0731@gmail.com>
2018-05-30 23:53:19 +0900

    Add a new navigation controller as an initial view controller on AppDelegate, which contains a view controller from Main Stroyboard.

--- Tutorials/Section_2/Section_2/AppDelegate.swift
+++ Tutorials/Section_2/Section_2/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let tmpStoryboad = UIStoryboard(name: "Main", bundle: nil)
+        let mainVC = tmpStoryboad.instantiateInitialViewController()
+        let mainNaviVC = UINavigationController(rootViewController: mainVC!)
+        
+        self.window?.rootViewController = mainNaviVC
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
