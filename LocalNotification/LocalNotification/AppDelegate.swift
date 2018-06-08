102d8d1ba0447e89657317e988ceacf810baf299
Yongseok Choi <clyksb0731@gmail.com>
2018-06-08 23:35:20 +0900

    Try to add latest user notification on AppDelegate.

--- LocalNotification/LocalNotification/AppDelegate.swift
+++ LocalNotification/LocalNotification/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { (granted, error) in
+                
+            })
+            
+        } else {
+            
+        }
+        
         return true
     }
 
