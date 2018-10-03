7f7eb0c98bdd0a4408b8233eec7f5c5aa1c24183
Yongseok Choi <clyksb0731@gmail.com>
2018-10-03 23:08:29 +0900

    Add new project 'ChatTableView' to practice chat table view

---
+++ ChatTableView_Sample/ChatTableView_Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatTableView_Sample
+//
+//  Created by 최용석 on 03/10/2018.
+//  Copyright © 2018 최용석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
