bd713530226e2a20cc906000f183c3f2d8c0b321
Yongseok Choi <clyksb0731@gmail.com>
2018-10-04 13:29:15 +0900

    Add text field for message and movement when the keyboard appears

---
+++ ChatTableView_Sample/ChatTableView_Sample/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  ChatTableView_Sample
+//
+//  Created by 최용석 on 04/10/2018.
+//  Copyright © 2018 최용석. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ChatTableView_Sample/ChatTableView_Sample/ViewController.swift
+++ ChatTableView_Sample/ChatTableView_Sample/ViewController.swift
@@ -11,37 +11,139 @@ import UIKit
 class ViewController: UIViewController {
 
     var chatTable: UITableView!
+    var chatTextField: UITextField!
     var chatButton: UIButton!
+    
+    var chatTextFieldBottom: NSLayoutConstraint?
+    var chatButtonBottom: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createViews()
+        setViews()
     }
     
     /// Create Views
     func createViews() {
-        self.chatTable = UITableView()
-        self.chatTable.delegate = self
-        self.chatTable.dataSource = self
+        self.chatTable = createChatTable()
+        self.view.addSubview(self.chatTable)
         
-        self.chatButton = UIButton()
+        self.chatTextField = createChatTextField()
+        self.view.addSubview(self.chatTextField)
+        
+        self.chatButton = createChatButton()
+        self.view.addSubview(self.chatButton)
     }
     
     /// Set Layout
     func setViews() {
+        let safeGuide: UILayoutGuide = self.view.safeAreaLayoutGuide
+        let marginGuide: UILayoutGuide = self.view.layoutMarginsGuide
         
+        // Chat Table Layout
+        self.chatTable.topAnchor.constraint(equalTo: safeGuide.topAnchor).isActive = true
+        self.chatTable.bottomAnchor.constraint(equalTo: self.chatTextField.topAnchor).isActive = true
+        self.chatTable.leadingAnchor.constraint(equalTo: safeGuide.leadingAnchor).isActive = true
+        self.chatTable.trailingAnchor.constraint(equalTo: safeGuide.trailingAnchor).isActive = true
+        
+        self.chatTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        self.chatTextField.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        chatTextFieldBottom =  self.chatTextField.bottomAnchor.constraint(equalTo: safeGuide.bottomAnchor)
+        chatTextFieldBottom?.isActive = true
+        self.chatTextField.leadingAnchor.constraint(equalTo: safeGuide.leadingAnchor).isActive = true
+        self.chatTextField.trailingAnchor.constraint(equalTo: self.chatButton.leadingAnchor).isActive = true
+        
+        self.chatButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        self.chatButton.topAnchor.constraint(equalTo: self.chatTable.bottomAnchor).isActive = true
+        chatButtonBottom = self.chatButton.bottomAnchor.constraint(equalTo: safeGuide.bottomAnchor)
+        chatButtonBottom?.isActive = true
+        self.chatButton.trailingAnchor.constraint(equalTo: safeGuide.trailingAnchor).isActive = true
+    }
+    
+    func createChatTable() -> UITableView {
+        let chatTable: UITableView = UITableView()
+        chatTable.delegate = self
+        chatTable.dataSource = self
+        chatTable.register(ChatCell.self, forCellReuseIdentifier: "MyCell")
+        chatTable.separatorStyle = .none
+        chatTable.layer.borderWidth = 1
+        chatTable.layer.borderColor = UIColor.gray.cgColor
+        
+        chatTable.translatesAutoresizingMaskIntoConstraints = false
+        
+        return chatTable
+    }
+    
+    func createChatTextField() -> UITextField {
+        let chatTextField: UITextField = UITextField()
+        chatTextField.delegate = self
+        chatTextField.layer.borderWidth = 1
+        chatTextField.layer.borderColor = UIColor.black.cgColor
+        chatTextField.layer.cornerRadius = 10
+        chatTextField.clipsToBounds = true
+        
+        // TODO: - Why not touchUpInside?
+        chatTextField.addTarget(self, action: #selector(touchTextField(_:)), for: .touchDown)
+        
+        chatTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return chatTextField
+    }
+    
+    func createChatButton() -> UIButton {
+        let chatButton: UIButton = UIButton()
+        chatButton.setTitle("Send", for: .normal)
+        chatButton.setTitle("Clicked", for: .highlighted)
+        chatButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        chatButton.setTitleColor(UIColor.black, for: .normal)
+        chatButton.setTitleColor(UIColor.gray, for: .highlighted)
+        chatButton.addTarget(self, action: #selector(clickButton(_:)), for: .touchUpInside)
+        
+        chatButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        return chatButton
+    }
+}
+
+extension ViewController {
+    @objc func touchTextField(_ sender: UITextField) {
+        self.chatTextFieldBottom?.constant = -300
+        self.chatButtonBottom?.constant = -300
+    }
+    
+    @objc func clickButton(_ sender: UIButton) {
+        self.chatTextFieldBottom?.constant = 0
+        self.chatButtonBottom?.constant = 0
+        self.chatTextField.resignFirstResponder()
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as! ChatCell
+        return cell
     }
     
-    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        self.chatTextFieldBottom?.constant = 0
+        self.chatButtonBottom?.constant = 0
+        
+        return true
+    }
 }
 
