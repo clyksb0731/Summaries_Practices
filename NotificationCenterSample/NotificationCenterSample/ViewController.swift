1ce52ed72b8a670529dc628bc519c5364c21c76f
Yongseok Choi <clyksb0731@gmail.com>
2019-04-26 01:01:43 +0900

    Add a project about NotificationCenter to learn about userInfo of Notification.

---
+++ NotificationCenterSample/NotificationCenterSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationCenterSample
+//
+//  Created by 최용석 on 26/04/2019.
+//  Copyright © 2019 최용석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
