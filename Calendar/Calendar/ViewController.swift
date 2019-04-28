19307020c95213cc8c967f68b7220f8bb98aa540
Yongseok Choi <clyksb0731@gmail.com>
2019-04-29 01:15:46 +0900

    Implement a completed collection view.

--- Calendar/Calendar/ViewController.swift
+++ Calendar/Calendar/ViewController.swift
@@ -10,18 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var calendarView: UICollectionView = {
-//        let calendarView = UICollectionView()
-//        calendarView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        return calendarView
-//    }()
+    var calendarView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = UICollectionView.ScrollDirection.vertical
+        let calendarView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        calendarView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+
+        return calendarView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.setDelegates()
-        
+        self.view.addSubview(self.calendarView)
+        self.setLayout()
+        self.setDelegates()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Set delegate
+    func setDelegates() {
+        self.calendarView.delegate = self
+        self.calendarView.dataSource = self
+    }
+    
+    // MARK: Set layout
+    func setLayout() {
+        NSLayoutConstraint.activate([
+            self.calendarView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.calendarView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            self.calendarView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.calendarView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+            ])
+    }
+}
+
+// MARK: Extension for methods added additionally
+extension ViewController {
+    func testMethod() {
         let date = Date()
         let date2 = Date(timeInterval: -86400, since: date)
         
@@ -33,36 +64,43 @@ class ViewController: UIViewController {
         let dateComponent = DateComponents(calendar: Calendar.current, year: 2009, month: 8, day: 10)
         print("Tmp Date:::::: ", dateComponent.date!)
         
-//        let tmpGDate = DateGenerator(date: Date())
-//        tmpGDate.tmpPrintDateComponent()
-//
-//        let tmpGTDate = DateGenerator(date: Date(timeInterval: -86400 * 180, since: Date()))
-//        tmpGTDate.tmpPrintDateComponent()
+        //        let tmpGDate = DateGenerator(date: Date())
+        //        tmpGDate.tmpPrintDateComponent()
+        //
+        //        let tmpGTDate = DateGenerator(date: Date(timeInterval: -86400 * 180, since: Date()))
+        //        tmpGTDate.tmpPrintDateComponent()
         
         print("TimeZone: ", TimeZone.current)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: Set delegate
-    func setDelegates() {
-//        self.calendarView.delegate = self
-//        self.calendarView.dataSource = self
     }
 }
 
 // MARK: Extension for collection view delegate and data source
-//extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        return UICollectionViewCell()
-//    }
-//
-//
-//}
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let label = UILabel(frame: cell.contentView.frame)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.text = String(indexPath.row)
+        cell.contentView.addSubview(label) // FIXME: Continuously adding.
+        cell.contentView.backgroundColor = .yellow
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (UIScreen.main.bounds.width - 10) / 2
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+}
