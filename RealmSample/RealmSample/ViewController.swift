cfe3f828c115df7c44760cced1244d8c8e707b19
Yongseok Choi <clyksb0731@gmail.com>
2019-06-26 23:40:47 +0900

    Add test of multiple queue for realm initialization.

--- RealmSample/RealmSample/ViewController.swift
+++ RealmSample/RealmSample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,56 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("will instantiate the first realm")
+        
+        self.realmTestOnSerialQueue()
+    }
 
 
+}
+
+// MARK: Extension for methods added
+extension ViewController {
+    func realmTestOnSerialQueue() {
+        let serialQueue = DispatchQueue(label: "serialQueue")
+        
+        serialQueue.async {
+            do {
+                let realm = try Realm()
+                print("did instantiate first Realm")
+                self.realmTestOnConcurrentQueue()
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+    func realmTestOnConcurrentQueue() {
+        print("will instantiate second Realm")
+        let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
+        
+        concurrentQueue.async {
+            do {
+                let realm = try Realm()
+                
+                let table = Table()
+                
+                try realm.write {
+                    realm.add(table)
+                }
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+            print("did instantiate second Realm")
+        }
+    }
 }
 
