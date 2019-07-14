c5221fcbaf571a238c568b75ccb288b55ad4ce33
Yongseok Choi <clyksb0731@gmail.com>
2019-07-14 20:20:36 +0900

    Test Observerble creation.

--- RxSwift_Sample/RxSwift_Sample/ViewController.swift
+++ RxSwift_Sample/RxSwift_Sample/ViewController.swift
@@ -15,33 +15,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var notificationLabel: UILabel!
     @IBOutlet weak var checkButton: UIButton!
     
+    var tableView = UITableView()
+    var cities = ["Seoul", "Daejeon"]
+    
     var disposeBag = DisposeBag()
     
-    var notificationRelay: BehaviorRelay = BehaviorRelay<String>(value: "")
+    var testObservable: Observable<String>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.bindUI()
-        self.bindRelay()        
     }
     
     func bindUI() {
-        self.checkButton.rx.tap
-            .subscribe(onNext: {
-                self.notificationRelay.accept("Test RxSwift")
-            })
-            .disposed(by: self.disposeBag)
+       self.testStringObservable()
+//        .filter {$0 == "Two"}
+        .bind(to: self.notificationLabel.rx.text)
+        .disposed(by: self.disposeBag)        
     }
     
-    func bindRelay() {
-        self.notificationRelay
-            .bind(to: self.notificationLabel.rx.text)
-            .disposed(by: self.disposeBag)
+    func testStringObservable() -> Observable<String> {
+        return Observable.create { (observer) -> Disposable in
+            var flag: Bool = false
+            
+            DispatchQueue.global().async {
+                for index in 1...10_000 {
+                    DispatchQueue.main.sync { [weak self] in
+                        self?.asyncCheckLabel.text = String(index)
+                    }
+                    
+                    if flag {
+                        break
+                    }
+                }
+                observer.onNext("AfterAsync")
+                observer.onCompleted()
+            }
+            
+            observer.onNext("One")
+            observer.onNext("Two")
+            observer.onNext("Three")
+            
+            return Disposables.create {
+                flag = true
+            }
+        }
     }
     
     @IBAction func checkAsyncButton(_ sender: UIButton) {
-        
+        self.bindUI()
+    }
+    
+    @IBAction func stopBinding(_ sender: Any) {
+        self.disposeBag = DisposeBag()
     }
 }
 
