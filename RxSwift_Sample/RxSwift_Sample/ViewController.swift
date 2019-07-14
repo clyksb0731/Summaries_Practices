ece7654965a78983b490c586edcfa2535f8c8692
Yongseok Choi <clyksb0731@gmail.com>
2019-07-14 18:32:32 +0900

    Use BehaviorRelay for UIButton.

--- RxSwift_Sample/RxSwift_Sample/ViewController.swift
+++ RxSwift_Sample/RxSwift_Sample/ViewController.swift
@@ -13,26 +13,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var asyncCheckLabel: UILabel!
     @IBOutlet weak var notificationLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
+    var disposeBag = DisposeBag()
+    
+    var notificationRelay: BehaviorRelay = BehaviorRelay<String>(value: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(postAsyncCheck(_:)), name: Notification.Name(rawValue: "asyncCheck"), object: nil)
-        
+        self.bindUI()
+        self.bindRelay()        
+    }
+    
+    func bindUI() {
+        self.checkButton.rx.tap
+            .subscribe(onNext: {
+                self.notificationRelay.accept("Test RxSwift")
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    func bindRelay() {
+        self.notificationRelay
+            .bind(to: self.notificationLabel.rx.text)
+            .disposed(by: self.disposeBag)
     }
     
     @IBAction func checkAsyncButton(_ sender: UIButton) {
-        // NotificationCenter's notifications observed work synchronously
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "asyncCheck"), object: nil)
-        self.asyncCheckLabel.text = "Check Button touched"
-    }
-    
-    @objc func postAsyncCheck(_ notification: Notification) {
-        for _ in 1...100_000_000 {
-            
-        }
         
-        self.notificationLabel.text = "After Notification"
     }
 }
 
