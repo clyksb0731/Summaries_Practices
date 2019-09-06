b22a0b435360202c99f101c816e0187d5853d2e5
Yongseok Choi <clyksb0731@gmail.com>
2019-09-06 20:30:08 +0900

    Add the new collection view sample project to practice collection view.

---
+++ CollectionView_Samples/CollectionView_Samples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView_Samples
+//
+//  Created by Yongseok Choi on 06/09/2019.
+//  Copyright © 2019 Yongseok Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
