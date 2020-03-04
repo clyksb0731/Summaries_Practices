d4c60bd4d685b6669bcb6ec4e9490bfcd1146dbb
Yongseok Choi <clyksb0731@gmail.com>
2020-03-04 23:13:40 +0900

    Create a PushKit sample which deal with incoming call using CallKit.

---
+++ PushKitSample/PushKitSample/AppDelegate.swift
@@ -0,0 +1,101 @@
+//
+//  AppDelegate.swift
+//  PushKitSample
+//
+//  Created by Yongseok Choi on 2020/03/03.
+//  Copyright © 2020 Yongseok Choi. All rights reserved.
+//
+
+import UIKit
+import PushKit
+import CallKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.voipRegistration()
+        
+        return true
+    }
+    
+    func voipRegistration() {
+        let voipRegistry: PKPushRegistry = PKPushRegistry(queue: DispatchQueue.main)
+        voipRegistry.delegate = self
+        voipRegistry.desiredPushTypes = [PKPushType.voIP]
+    }
+}
+
+// MARK: PushKit Delegate
+extension AppDelegate: PKPushRegistryDelegate {
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        let deviceTokenString = pushCredentials.token.reduce("", {$0 + String(format: "%02X", $1)})
+        print("pushRegistry -> deviceToken :\(deviceTokenString)")
+        
+        let deviceToken = pushCredentials.token.map { String(format: "%02x", $0) }.joined()
+        print("pushRegistry -> deviceToken :\(deviceToken)")
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+        print("Push registry failed")
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        
+        print(payload.dictionaryPayload)
+        print("Push Type: \(type)")
+        
+        let config = CXProviderConfiguration(localizedName: "My App")
+        config.iconTemplateImageData = UIImage(named: "Neo")!.pngData()
+//        config.ringtoneSound = "ringtone.caf"
+        config.includesCallsInRecents = false;
+        config.supportsVideo = true;
+        let provider = CXProvider(configuration: config)
+        provider.setDelegate(self, queue: nil)
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .generic, value: "Pete Za")
+        update.hasVideo = true
+        provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
+        
+        completion()
+    }
+}
+
+extension AppDelegate: CXProviderDelegate {
+    func providerDidReset(_ provider: CXProvider) {
+        print("Did Reset")
+    }
+    
+    func providerDidBegin(_ provider: CXProvider) {
+        print("Did Begin")
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXStartCallAction) {
+        action.fulfill()
+        print("Start")
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+        action.fulfill()
+        print("Answer")
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        action.fulfill()
+        print("End")
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXSetMutedCallAction) {
+        action.fulfill()
+        print("Muted")
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXSetHeldCallAction) {
+        action.fulfill()
+        print("Held call")
+    }
+}
+
---
+++ PushKitSample/PushKitSample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PushKitSample
+//
+//  Created by Yongseok Choi on 2020/03/03.
+//  Copyright © 2020 Yongseok Choi. All rights reserved.
+//
+
+import UIKit
+import CallKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+        
+    }
+
+    
+}
+
