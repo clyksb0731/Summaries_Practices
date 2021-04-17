c827d4cb1a66c694f2c083fbb902dac2602597b4
Yongseok Choi <clyksb0731@gmail.com>
2021-04-17 22:33:59 +0900

    Create a new project to test subview.

---
+++ SubviewTest/SubviewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubviewTest
+//
+//  Created by Yongseok Choi on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
