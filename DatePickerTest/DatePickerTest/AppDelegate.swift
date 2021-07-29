bc46505f197dabd208765db28a16a21c48f7a0b9
Yongseok Choi <clyksb0731@gmail.com>
2021-07-29 14:32:38 +0900

    Create a new project to practice UIDatePicker.

---
+++ DatePickerTest/DatePickerTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DatePickerTest
+//
+//  Created by Yongseok Choi on 2021/07/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ DatePickerTest/DatePickerTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DatePickerTest
+//
+//  Created by Yongseok Choi on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var yearAndMonthDatePicker: UIDatePicker!
+    @IBOutlet weak var timeDatePIcker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yearAndMonthDatePicker(_ sender: UIDatePicker) {
+        
+    }
+    
+
+    @IBAction func timeDatePicker(_ sender: UIDatePicker) {
+        
+    }
+}
+
