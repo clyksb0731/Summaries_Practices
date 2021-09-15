aa616072f6b34ab9b0bf0d8581f682129de8a945
Yongseok Choi <clyksb0731@gmail.com>
2021-09-15 18:16:53 +0900

    Create a new project to practice CoreBluetooth.

---
+++ CoreBluetoothPractice/CoreBluetoothPractice/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CoreBluetoothPractice
+//
+//  Created by Yongseok Choi on 2021/09/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ CoreBluetoothPractice/CoreBluetoothPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreBluetoothPractice
+//
+//  Created by Yongseok Choi on 2021/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
